cce7c7555386bd7e82fdedc1152e656e42da73d6
Ross Claytor <johnrossclaytor@gmail.com>
2017-09-09 12:30:08 -0500

    initial commit

--- rats.py
+++ rats.py
@@ -1,16 +1,11 @@
 from flask import Flask
-from flask.ext.login import LoginManager
-from flask.ext.login import login_required
-from flask.ext.login import login_user
-from flask.ext.login import logout_user
-from flask.ext.login import current_user
 from flask import render_template
 from flask import redirect
 from flask import request
 from flask import url_for
 
 app = Flask(__name__)
-login_manager = LoginManager(app)
+
 
 @app.route("/")
 def home():
